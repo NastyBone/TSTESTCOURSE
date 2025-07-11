c4052a350b8c3b5aa406b87c00612da5f61e4ac2
José Prieto <42517974+NastyBone@users.noreply.github.com>
2025-07-11 19:51:51 -0400

    23 - Passcheck iteration 2

--- src/app/password_checker/PasswordChecker.ts
+++ src/app/password_checker/PasswordChecker.ts
@@ -1,14 +1,28 @@
+export enum PasswordErrors {
+    SHORT = 'Password requires to have at least 8 characters',
+    NO_LOWER_CASE = 'Password requires to have at leas one lower case character',
+    NO_UPPER_CASE = 'Password requires to have at leas one upper case character',
+}
+export interface CheckResult {
+    valid: boolean;
+    reasons: PasswordErrors[];
+}
+
 export class PasswordChecker {
-    public PasswordChecker(password: string): boolean {
+    public PasswordChecker(password: string): CheckResult {
+        const reasons: PasswordErrors[] = [];
         if (password.length < 8) {
-            return false;
+            reasons.push(PasswordErrors.SHORT);
         }
         if (password === password.toUpperCase()) {
-            return false; 
+            reasons.push(PasswordErrors.NO_LOWER_CASE); 
         }
         if (password === password.toLowerCase()) {
-            return false;
+            reasons.push(PasswordErrors.NO_UPPER_CASE)
         }
-        return true;
+        return {
+            valid: reasons.length < 1,
+            reasons,
+        }
     }
 }
--- src/test/password_checker/PasswordChecker.test.ts
+++ src/test/password_checker/PasswordChecker.test.ts
@@ -1,4 +1,4 @@
-import { PasswordChecker } from "../../app/password_checker/PasswordChecker";
+import { PasswordChecker, PasswordErrors } from "../../app/password_checker/PasswordChecker";
 
 describe('Testing for password checker', () => {
     let sut: PasswordChecker
@@ -10,30 +10,38 @@ describe('Testing for password checker', () => {
     describe('First iteration', () => {
         // Has to have more than 8 characters
         it('Should be valid a password with 8 characters', () => {
-            const actual = sut.PasswordChecker('12345678aA');
-            expect(actual).toBe(true);
+            const actual = sut.PasswordChecker('12345678');
+            expect(actual.reasons).not.toContain(PasswordErrors.SHORT);
         })
         it('Should fail on password with less than 8 characters length', () => {
             const actual = sut.PasswordChecker('1234567');
-            expect(actual).toBe(false);
+            expect(actual.valid).toBe(false);
+            expect(actual.reasons).toContain(PasswordErrors.SHORT);
         })
         // Should have at least one uppercase character
         it('Should be valid to have one lowercase letter', () => {
-            const actual = sut.PasswordChecker('1234567aA');
-            expect(actual).toBe(true);
+            const actual = sut.PasswordChecker('aBCD');
+            expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE);
         })
         it('Should fail on not having at least one lowercase letter', () => {
-            const actual = sut.PasswordChecker('1234567A');
-            expect(actual).toBe(false);
+            const actual = sut.PasswordChecker('ABCD');
+            expect(actual.valid).toBe(false);
+            expect(actual.reasons).toContain(PasswordErrors.NO_LOWER_CASE)
         })
         // Should have at least one lowecase character
         it('Should be valid to have one uppecarse letter', () => {
-            const actual = sut.PasswordChecker('1234567aA');
-            expect(actual).toBe(true);
+            const actual = sut.PasswordChecker('Abcd');
+            expect(actual.reasons).not.toContain(PasswordErrors.NO_UPPER_CASE);
         })
         it('Should fail on not having at least one uppercase letter', () => {
-            const actual = sut.PasswordChecker('1234567a');
-            expect(actual).toBe(false);
+            const actual = sut.PasswordChecker('abcd');
+            expect(actual.valid).toBe(false);
+            expect(actual.reasons).toContain(PasswordErrors.NO_UPPER_CASE);
+        })
+        it('Should be valid on complex and correct passwords', () => {
+            const actual = sut.PasswordChecker('Abc123456')
+            expect(actual.valid).toBe(true);
+            expect(actual.reasons).toHaveLength(0)
         })
     })
 })
