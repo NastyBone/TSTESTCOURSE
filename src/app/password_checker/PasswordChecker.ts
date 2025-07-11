8f22b32eb981e9e5c190f849fa832bc4dfffc8a3
José Prieto <42517974+NastyBone@users.noreply.github.com>
2025-07-11 19:16:37 -0400

    22 - passcheck iteration 1

--- src/app/password_checker/PasswordChecker.ts
+++ src/app/password_checker/PasswordChecker.ts
@@ -1,5 +1,14 @@
 export class PasswordChecker {
-    public PasswordChecker() {
-        return;
+    public PasswordChecker(password: string): boolean {
+        if (password.length < 8) {
+            return false;
+        }
+        if (password === password.toUpperCase()) {
+            return false; 
+        }
+        if (password === password.toLowerCase()) {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/password_checker/PasswordChecker.test.ts
+++ src/test/password_checker/PasswordChecker.test.ts
@@ -6,7 +6,34 @@ describe('Testing for password checker', () => {
     beforeEach(() => {
         sut = new PasswordChecker()
     })
-    it('Should do nothing', () => {
-        sut.PasswordChecker();
+
+    describe('First iteration', () => {
+        // Has to have more than 8 characters
+        it('Should be valid a password with 8 characters', () => {
+            const actual = sut.PasswordChecker('12345678aA');
+            expect(actual).toBe(true);
+        })
+        it('Should fail on password with less than 8 characters length', () => {
+            const actual = sut.PasswordChecker('1234567');
+            expect(actual).toBe(false);
+        })
+        // Should have at least one uppercase character
+        it('Should be valid to have one lowercase letter', () => {
+            const actual = sut.PasswordChecker('1234567aA');
+            expect(actual).toBe(true);
+        })
+        it('Should fail on not having at least one lowercase letter', () => {
+            const actual = sut.PasswordChecker('1234567A');
+            expect(actual).toBe(false);
+        })
+        // Should have at least one lowecase character
+        it('Should be valid to have one uppecarse letter', () => {
+            const actual = sut.PasswordChecker('1234567aA');
+            expect(actual).toBe(true);
+        })
+        it('Should fail on not having at least one uppercase letter', () => {
+            const actual = sut.PasswordChecker('1234567a');
+            expect(actual).toBe(false);
+        })
     })
 })
