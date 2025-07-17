e72abd9bd367005a87bc74cee0cadfd2188e0e18
José Prieto <42517974+NastyBone@users.noreply.github.com>
2025-07-16 21:08:24 -0400

    29 - mocks

--- src/test/doubles/OtherUtils.ts
+++ src/test/doubles/OtherUtils.ts
@@ -1,6 +1,55 @@
 import { calculateStringComplexity, toUppercaseWithCb } from "../../app/doubles/OtherUtils"
 
 describe('OtherUtils test suite', () => {
+
+    describe('tracking calls', () => {
+        let argsCb = [];
+        let timesCalled = 0;
+
+        afterEach(() => {
+            argsCb = [];
+            timesCalled = 0;
+        })
+        const trackingFunction = (arg: string) => {
+            argsCb.push(arg);
+            timesCalled++;
+        }
+
+        it('should return valid uppercase with callback', () => {
+            const actual = toUppercaseWithCb('abc', trackingFunction);
+            expect(actual).toBe('ABC');
+            expect(argsCb).toContain('Tested with string abc')
+            expect(timesCalled).toEqual(1);
+        })
+        it('should return invalid uppercase with callback', () => {
+            const actual = toUppercaseWithCb('', trackingFunction);
+            expect(actual).toBeUndefined();
+            expect(argsCb).toContain('Invalid String')
+            expect(timesCalled).toEqual(1);
+        })
+
+    })
+
+    describe('tracking calls with jest', () => {
+
+        afterEach(() => {
+            trackingFunction = jest.fn();
+        })
+        let trackingFunction = jest.fn();
+
+        it('should return valid uppercase with callback', () => {
+            const actual = toUppercaseWithCb('abc', trackingFunction);
+            expect(actual).toBe('ABC');
+            expect(trackingFunction).toHaveBeenCalledWith('Tested with string abc')
+            expect(trackingFunction).toHaveBeenCalledTimes(1);
+        })
+        it('should return invalid uppercase with callback', () => {
+            const actual = toUppercaseWithCb('', trackingFunction);
+            expect(actual).toBeUndefined();
+            expect(trackingFunction).toHaveBeenCalledWith('Invalid String')
+            expect(trackingFunction).toHaveBeenCalledTimes(1);
+        })
+    })
     it('should calculate complexity of a stringObject correctly', () => {
         const stub = {
             length: 5,
@@ -15,11 +64,11 @@ describe('OtherUtils test suite', () => {
 
     // In fakes, jest mislead the behaviour of the callback, being it invalid but passes coverage
     it('should return valid uppercase with callback', () => {
-        const actual = toUppercaseWithCb('abc', ()=>{});
+        const actual = toUppercaseWithCb('abc', () => { });
         expect(actual).toBe('ABC');
     })
     it('should return invalid uppercase with callback', () => {
-        const actual = toUppercaseWithCb('', ()=>{});
+        const actual = toUppercaseWithCb('', () => { });
         expect(actual).toBeUndefined();
     })
 })
