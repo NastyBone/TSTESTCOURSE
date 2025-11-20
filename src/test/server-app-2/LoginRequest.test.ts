426c8d274ea10db834a7a0286b12b011f69c86af
José Prieto <42517974+NastyBone@users.noreply.github.com>
2025-11-20 00:38:45 -0400

    48. Completing login request test suite

--- src/test/server-app-2/LoginRequest.test.ts
+++ src/test/server-app-2/LoginRequest.test.ts
@@ -1,3 +1,6 @@
+import { DataBase } from "../../app/server_app/data/DataBase";
+import { HTTP_CODES, HTTP_METHODS } from "../../app/server_app/model/ServerModel";
+import { Server } from "../../app/server_app/server/Server";
 import { RequestTestWrapper } from "./utils/RequestTestWrapper";
 import { ResponseTestWrapper } from "./utils/ResponseTestWrapper";
 
@@ -15,11 +18,84 @@ jest.mock('http', () => ({
     }
 }))
 
+const spyOnGetBy = jest.spyOn(DataBase.prototype, 'getBy');
+const spyOnInsert = jest.spyOn(DataBase.prototype, 'insert')
+
 describe('Test suite for Login Requests', () => {
-    beforeEach(() => {
+    const someAccount = {
+        userName: 'userName',
+        password: 'password',
+    }
+    const someToken = 'someToken';
+
+    afterEach(() => {
         requestWrapper.clearFields();
         responseWrapper.clearFields();
         jest.resetAllMocks();
+    })
+    beforeEach(() => {
+        requestWrapper.headers["user-agent"] = "Windows"
+    })
+
+    // Login Success
+    it('Should login successfully with valid credentials', async () => {
+        requestWrapper.url = 'localhost:8080/login';
+        requestWrapper.method = HTTP_METHODS.POST;
+        requestWrapper.body = someAccount;
+        spyOnGetBy.mockResolvedValueOnce(someAccount)
+        spyOnInsert.mockResolvedValueOnce(someToken);
+
+
+        await new Server().startServer();
+        await new Promise(process.nextTick);
+
+        expect(responseWrapper.statusCode).toBe(HTTP_CODES.CREATED);
+        expect(responseWrapper.body).toEqual({ token: someToken })
+
+    })
+
+    // Not found case
+    it('Should not login if credentials are not valid', async () => {
+        requestWrapper.url = 'localhost:8080/login';
+        requestWrapper.method = HTTP_METHODS.POST;
+        requestWrapper.body = {
+            userName: 'OtherUsername',
+            password: 'OtherPassword',
+        };
+        spyOnGetBy.mockResolvedValueOnce({
+            userName: 'OtherUsername',
+            password: 'OtherPassword',
+        })
+
+        await new Server().startServer();
+        await new Promise(process.nextTick);
+
+        expect(responseWrapper.statusCode).toBe(HTTP_CODES.NOT_fOUND);
+        expect(responseWrapper.body).toEqual('wrong username or password')
+
+    })
+
+    // Bad request case
+    it('Should throw error if credentials missing', async () => {
+        requestWrapper.url = 'localhost:8080/login';
+        requestWrapper.method = HTTP_METHODS.POST;
+        requestWrapper.body = {};
+        await new Server().startServer();
+        await new Promise(process.nextTick);
+        expect(responseWrapper.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
+        expect(responseWrapper.body).toEqual('userName and password required')
+    })
+
+    // No supported methods
+    it('Should do nothing on not handled routes', async () => {
+        requestWrapper.url = 'localhost:8080/login';
+        requestWrapper.method = HTTP_METHODS.DELETE;
+
+        await new Server().startServer();
+        await new Promise(process.nextTick);
+
+        expect(responseWrapper.statusCode).toBeUndefined();
+        expect(responseWrapper.body).toBeUndefined();
     })
 
 })
